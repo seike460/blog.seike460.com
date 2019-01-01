8a86205331946b87eaaee8667925aadd6212da23
seike460 <seike460@gmail.com>
2019-01-02 01:56:00 +0900

    [ci skip]:+1: ogimage

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -35,6 +35,10 @@ function SEO({ description, lang, meta, keywords, title }) {
                 content: `website`,
               },
               {
+                property: `og:image`,
+                  content: `/icons/icon-512x512.png`,
+              },
+              {
                 name: `twitter:card`,
                 content: `summary`,
               },
